21ddb5a39c271bf6298022217bfb9a1897712a99
Nahian I. Nafseen <ninx.sh@gmail.com>
2026-08-22 19:35:58 +0600

    Add Alt+Space support override activate-window-menu keybinding
    Research confirmed:
    - alt space is defined in org.gnome.desktop.wm.keybindings as activate-window-menu
    - default value is ['<Alt>space'] handled by mutter at low level
    - can be disabled by setting to empty array via gsettings
    - grab_accelerator should take priority over normal wm keybindings
      similar to powertoys run low level keyboard hook on windows
    - but to guarantee it works we also disable the wm binding as fallback
    
    Implementation in keybinding.js:
    - import Gio to access wm keybinding settings
    - in listenFor try grab_accelerator first
    - if it fails AND accelerator is '<Alt>space' save original
      activate-window-menu value disable it via set_strv([]) and retry grab
    - in unlisten and disable restore original value if we changed it
    - this guarantees alt space works exactly like powertoys run on windows
      the window menu is disabled while spotlight uses alt space
      and automatically restored when the shortcut changes or extension disables
    
    Users can now set alt space as their spotlight shortcut in preferences
    just like powertoys run on windows 11

--- keybinding.js
+++ keybinding.js
@@ -3,12 +3,23 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
+import Gio from 'gi://Gio';
 
 // grabs keys via mutter instead of gsettings
 // more reliable than addkeybinding which can fail if schema is not ready at enable time
+//
+// for alt space we handle a conflict with mutter activate-window-menu
+// which defaults to alt space powertoys run on windows solves the same problem
+// with a low level keyboard hook on gnome grab_accelerator serves the same role
+// but if the grab fails we temporarily disable activate-window-menu via gsettings
+// and restore it on disable this matches what users expect from powertoys
 export class KeybindingManager {
     enable() {
         this._grabbers = {};
+        this._wmSettings = new Gio.Settings({
+            schema_id: 'org.gnome.desktop.wm.keybindings',
+        });
+        this._savedWindowMenu = null;
         global.display.connectObject('accelerator-activated', (_, action) => {
             const grabber = this._grabbers[action];
             if (grabber)
@@ -18,11 +29,28 @@ export class KeybindingManager {
     disable() {
         this.unlisten();
         global.display.disconnectObject(this);
+        // restore window menu keybinding if we disabled it
+        if (this._savedWindowMenu !== null) {
+            this._wmSettings.set_strv('activate-window-menu', this._savedWindowMenu);
+            this._savedWindowMenu = null;
+        }
+        this._wmSettings = null;
     }
     listenFor(accelerator, callback) {
-        const action = global.display.grab_accelerator(accelerator, 0);
+        let action = global.display.grab_accelerator(accelerator, 0);
+
+        // alt space conflicts with mutter activate-window-menu default
+        // if grab failed and this is alt space temporarily disable the wm binding
+        if (action === Meta.KeyBindingAction.NONE && accelerator === '<Alt>space') {
+            this._savedWindowMenu = this._wmSettings.get_strv('activate-window-menu');
+            this._wmSettings.set_strv('activate-window-menu', []);
+            // try grab again now that wm binding is disabled
+            action = global.display.grab_accelerator(accelerator, 0);
+        }
+
         if (action === Meta.KeyBindingAction.NONE)
             return false;
+
         const name = Meta.external_binding_name_for_action(action);
         Main.wm.allowKeybinding(name, Shell.ActionMode.ALL);
         this._grabbers[action] = {name, accelerator, callback};
@@ -34,5 +62,10 @@ export class KeybindingManager {
             global.display.ungrab_accelerator(parseInt(k, 10));
         }
         this._grabbers = {};
+        // restore window menu keybinding if we disabled it
+        if (this._savedWindowMenu !== null) {
+            this._wmSettings.set_strv('activate-window-menu', this._savedWindowMenu);
+            this._savedWindowMenu = null;
+        }
     }
 }
